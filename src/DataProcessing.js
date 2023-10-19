1d2b24b9224e245d335cf4ffe8c84c2bf42b21a0
tanzidakazi <tanzidakazi21@gmail.com>
2023-10-19 20:33:39 +1100

    babel.config.js

--- src/DataProcessing.js
+++ src/DataProcessing.js
@@ -2,10 +2,64 @@ import logo from './logo.svg';
 import './App.css';
 import Navigation from "./Navigation";
 
-function DataProcessing() {
-    return (
-        <p>content</p>
-    );
-}
+import React, { useState } from 'react';
+import { Button, Upload, Select, Space, Typography } from 'antd';
+import { UploadOutlined } from '@ant-design/icons';
+
+const { Title } = Typography;
+const { Option } = Select;
+
+const DataProcessing = () => {
+  const [file, setFile] = useState(null);
+  const [fileExtension, setFileExtension] = useState('.txt');
+
+  const handleFileChange = (info) => {
+    if (info.file.status === 'done') {
+      setFile(info.file.originFileObj);
+    }
+  };
+
+  const handleExtensionChange = (value) => {
+    setFileExtension(value);
+  };
+
+  const handleShowOutput = () => {
+    // Perform data processing here using the file and extension
+    console.log('File:', file);
+    console.log('File Extension:', fileExtension);
+    // Implement your data processing logic here
+  };
+
+  return (
+    <div style={{ padding: '20px' }}>
+      <Title level={2}>Data Processing</Title>
+      <Space direction="vertical" size="large">
+        <Space size="middle">
+          <span>Data Import:</span>
+          <Upload
+            customRequest={() => {}}
+            showUploadList={false}
+            onChange={handleFileChange}
+          >
+            <Button icon={<UploadOutlined />} >Upload File</Button>
+          </Upload>
+        </Space>
+        <Space size="middle">
+          <span>File Extension:</span>
+          <Select
+            defaultValue=".txt"
+            onChange={handleExtensionChange}
+          >
+            <Option value=".txt">.txt</Option>
+            <Option value=".csv">.csv</Option>
+          </Select>
+        </Space>
+        <Button type="primary" onClick={handleShowOutput}>
+          Show Output
+        </Button>
+      </Space>
+    </div>
+  );
+};
 
 export default DataProcessing;
--- src/ERGMAnalysis.js
+++ src/ERGMAnalysis.js
@@ -2,10 +2,87 @@ import logo from './logo.svg';
 import './App.css';
 import Navigation from "./Navigation";
 
-function ERGMAnalysis() {
-    return (
-        <p>content</p>
-    );
-}
+import React from 'react';
+import { Form, Input, Radio, Checkbox, Upload, Button } from 'antd';
+import { PlusOutlined } from '@ant-design/icons';
+
+const ERGMAnalysis = () => {
+  return (
+    <div style={{ padding: '20px' }}>
+      <h1>ERGM Analysis</h1>
+      <Form labelCol={{ span: 4 }} wrapperCol={{ span: 14 }}>
+        <Form.Item label="Node A">
+          <Input />
+        </Form.Item>
+        <Form.Item label="Node B">
+          <Input />
+        </Form.Item>
+        <Form.Item label="Choose One">
+          <Radio.Group>
+            <Radio value="simulation">Simulation</Radio>
+            <Radio value="estimation">Estimation</Radio>
+            <Radio value="validation">Validation</Radio>
+          </Radio.Group>
+        </Form.Item>
+        <Form.Item label="Select Options">
+          <Checkbox.Group>
+            <Checkbox value="include">Include</Checkbox>
+            <Checkbox value="directed">Directed</Checkbox>
+            <Checkbox value="fixed">Fixed</Checkbox>
+            <Checkbox value="fixedDensity">Fixed Density</Checkbox>
+          </Checkbox.Group>
+        </Form.Item>
+        <Form.Item label="Parameter">
+          <Form.Item
+            name="parameterFile"
+            valuePropName="fileList"
+            getValueFromEvent={(e) => e.fileList}
+            noStyle
+          >
+            <Upload
+              action="/upload"
+              listType="picture-card"
+              fileList={[]}
+            >
+              <Button icon={<PlusOutlined />}>Add File</Button>
+            </Upload>
+          </Form.Item>
+        </Form.Item>
+        <Form.Item label="Network File">
+          <Form.Item
+            name="networkFile"
+            valuePropName="fileList"
+            getValueFromEvent={(e) => e.fileList}
+            noStyle
+          >
+            <Upload
+              action="/upload"
+              listType="picture-card"
+              fileList={[]}
+            >
+              <Button icon={<PlusOutlined />}>Add File</Button>
+            </Upload>
+          </Form.Item>
+        </Form.Item>
+        <Form.Item label="Attribute File">
+          <Form.Item
+            name="attributeFile"
+            valuePropName="fileList"
+            getValueFromEvent={(e) => e.fileList}
+            noStyle
+          >
+            <Upload
+              action="/upload"
+              listType="picture-card"
+              fileList={[]}
+            >
+              <Button icon={<PlusOutlined />}>Add File</Button>
+            </Upload>
+          </Form.Item>
+        </Form.Item>
+      </Form>
+    </div>
+  );
+};
 
 export default ERGMAnalysis;
